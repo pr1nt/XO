cb1787d6ff46309be68ebde3b5cde6ae28b55824
pr1nt <printerrr.lol@gmail.com>
2016-06-08 02:56:10 +0500

    Working version with description

--- XO.cpp
+++ XO.cpp
@@ -6,20 +6,18 @@
 
 using namespace std;
 
+void ResultCheck();
 void Init();
 void Gameplay();
-bool Repeat();
+char field[3][3];
 void PrintField();
 int WinCheck();
-
-char field[3][3];
 int turn = 0;
-
-
 int main()
 {
 	Init();
 	Gameplay();
+	system("pause");
     return 0;
 }
 
@@ -36,12 +34,7 @@ void Gameplay() {
 	int row, column;
 	cout << "First player - X, second player - O\n";
 	cout << "Input rows and columns with space, like 1 2\n";
-	PrintField();
 	while (WinCheck() == 4){
-		if (curPlayer == 1) {
-			cout << "X turn: ";
-		}
-		else cout << "O turn: ";
 		cin >> row >> column;
 		if (curPlayer == 1) field[row - 1][column - 1] = 'X';
 		else				field[row - 1][column - 1] = 'O';
@@ -50,65 +43,41 @@ void Gameplay() {
 		PrintField();
 	}
 	switch (WinCheck()) {
-		case 1: {
-			cout << "First player won, repeat? (y/n)\n";
-			break;
-		}
-		case 2: {
-			cout << "Second player won, repeat? (y/n)\n";
-			break;
-		}
-		case 3: {
-			cout << "Draw, repeat? (y/n)\n";
-			break;
-		}
-	}
-	if (Repeat() == true) {
-		turn = 0;
-		Init();
-		Gameplay();
+		case 1: cout << "First player won";
+		case 2: cout << "Second player won";
+		case 3: cout << "Draw";
 	}
 }
 
 void PrintField() {
-	cout << "-" << field[0][0] << "-|-" << field[0][1] << "-|-" << field[0][2] << "-\n";
-	cout << "-" << field[1][0] << "-|-" << field[1][1] << "-|-" << field[1][2] << "-\n";
-	cout << "-" << field[2][0] << "-|-" << field[2][1] << "-|-" << field[2][2] << "-\n";
+	cout << field[0][0] << " | " << field[0][1] << " | " << field[0][2] << "\n";
+	cout << field[1][0] << " | " << field[1][1] << " | " << field[1][2] << "\n";
+	cout << field[2][0] << " | " << field[2][1] << " | " << field[2][2] << "\n";
 }
 
 int WinCheck() {
-	
-	if ((field[0][0] == 'X' && field[0][1] == 'X' && field[0][2] == 'X') ||
-		(field[1][0] == 'X' && field[1][1] == 'X' && field[1][2] == 'X') ||
-		(field[2][0] == 'X' && field[2][1] == 'X' && field[2][2] == 'X') ||
-		(field[0][0] == 'X' && field[1][0] == 'X' && field[2][0] == 'X') ||
-		(field[0][1] == 'X' && field[1][1] == 'X' && field[2][1] == 'X') ||
-		(field[0][2] == 'X' && field[1][2] == 'X' && field[2][2] == 'X') ||
-		(field[0][0] == 'X' && field[1][1] == 'X' && field[2][2] == 'X') ||
-		(field[0][2] == 'X' && field[1][1] == 'X' && field[2][0] == 'X')) {
+	if (('X' == field[0][0] == field[0][1] == field[0][2]) ||
+		('X' == field[1][0] == field[1][1] == field[1][2]) ||
+		('X' == field[2][0] == field[2][1] == field[2][2]) ||
+		('X' == field[0][0] == field[1][0] == field[2][0]) ||
+		('X' == field[0][1] == field[1][1] == field[2][1]) ||
+		('X' == field[0][2] == field[1][2] == field[2][2]) ||
+		('X' == field[0][0] == field[1][1] == field[2][2]) ||
+		('X' == field[0][2] == field[1][1] == field[2][0])) {
 		return 1;
 	}
-	else if ((field[0][0] == 'O' && field[0][1] == 'O' && field[0][2] == 'O') ||
-		(field[1][0] == 'O' && field[1][1] == 'O' && field[1][2] == 'O') ||
-		(field[2][0] == 'O' && field[2][1] == 'O' && field[2][2] == 'O') ||
-		(field[0][0] == 'O' && field[1][0] == 'O' && field[2][0] == 'O') ||
-		(field[0][1] == 'O' && field[1][1] == 'O' && field[2][1] == 'O') ||
-		(field[0][2] == 'O' && field[1][2] == 'O' && field[2][2] == 'O') ||
-		(field[0][0] == 'O' && field[1][1] == 'O' && field[2][2] == 'O') ||
-		(field[0][2] == 'O' && field[1][1] == 'O' && field[2][0] == 'O')) {
+	else if (('O' == field[0][0] == field[0][1] == field[0][2]) ||
+		('O' == field[1][0] == field[1][1] == field[1][2]) ||
+		('O' == field[2][0] == field[2][1] == field[2][2]) ||
+		('O' == field[0][0] == field[1][0] == field[2][0]) ||
+		('O' == field[0][1] == field[1][1] == field[2][1]) ||
+		('O' == field[0][2] == field[1][2] == field[2][2]) ||
+		('O' == field[0][0] == field[1][1] == field[2][2]) ||
+		('O' == field[0][2] == field[1][1] == field[2][0])) {
 		return 2;
 	}
 	else if (turn == 9){
 		return 3;
 	}
 	else return 4;
-}
-
-bool Repeat() {
-	char repeat;
-	cin >> repeat;
-	if (repeat == 'y') {
-		return true;
-	}
-	else return false;
 }
